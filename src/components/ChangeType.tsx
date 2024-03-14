766a52324cfd7e982313889e7583da818d9827c8
JoshuCW <joshwash@udel.edu>
2024-03-13 21:30:09 -0400

    Done

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,19 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    const [current, setCurrent] = useState<string>("short_answer_question");
-    const toggle = () => {
-        setCurrent(
-            current === "multiple_choice_question"
-                ? "short_answer_question"
-                : "multiple_choice_question"
-        );
-    };
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function flipType(): void {
+        if (questionType === "short_answer_question") {
+            setQuestionType("multiple_choice_question");
+        } else {
+            setQuestionType("short_answer_question");
+        }
+    }
+
     return (
         <div>
-            <Button onClick={toggle}>Change Type</Button>
-            {current === "multiple_choice_question" && <p> Multiple Choice</p>}
-            {current === "short_answer_question" && <p>Short Answer</p>}
+            <Button onClick={flipType}>Change Type</Button>
+            {questionType === "multiple_choice_question" && (
+                <p>Multiple Choice</p>
+            )}
+            {questionType === "short_answer_question" && <p>Short Answer</p>}
         </div>
     );
 }
